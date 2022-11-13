cb34c8245ff31ed2b42b7587e8e7fcd189ea6a60
Eli Frankel <101991821+elifrankel21@users.noreply.github.com>
2022-11-13 21:35:37 +0000

    backup for testing

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ var port = process.env['port']
 const app = express();
 const { readFile } = require('fs').promises;
 const fs = require('fs');
+const data = require("./data.json");
 // welcome page
+function errcheck(req,res){
+  if (err) {
+    res.status(500).send('<h1>Error! Code 500 server error go home or reload')
+  }
+}
 app.get('/', async (request, response) => {
-
     response.send( await readFile('./index.html', 'utf8') );
 });
 app.get('/style.css', function(req, res) {
@@ -15,7 +20,10 @@ app.get('/style.css', function(req, res) {
 app.listen(port, () => {
   console.log('server started on port', port);
 });
+app.get('/morework', async (request, response) => {
 
-//let student = response.data
-//let data = JSON.stringify(student);
-//fs.writeFileSync('data.json', data);
+    response.send( await readFile('./morework/index.html', 'utf8') );
+});
+app.get('*', function(req, res){
+   res.status(404).send('<h1>Error 404 go home</h1>');
+});
